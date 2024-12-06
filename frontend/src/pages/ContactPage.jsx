2a16cd938c15e1c8a9c92cf319c7ce141f93ed2e
Furkan ERDEM <furkanrdmm@gmail.com>
2024-12-06 08:04:07 +0300

    Auth page sayfası login ve register kısımları yapıldı.

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Header from '../components/Layout/Header/Header'
-import Contact from '../components/Contact/Contact'
-import Policy from '../components/Layout/Policy/Policy'
-import Footer from '../components/Layout/Footer/Footer'
+import React from "react";
+import Header from "../components/Layout/Header/Header";
+import Contact from "../components/Contact/Contact";
+
+import Footer from "../components/Layout/Footer/Footer";
 
 const ContactPage = () => {
   return (
     <React.Fragment>
-        <Header/>
-        <Contact/>
-        <Policy/>
-        <Footer/>
+      <Header />
+      <Contact />
 
-        
+      <Footer />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
